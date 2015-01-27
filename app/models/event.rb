dc65aa10e15b274f3c5ba50cd11e9b75f5362d75
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-27 11:22:11 -0800

    add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 class Event < ActiveRecord::Base
-	belongs_to :creator,   :class_name  => "User"
-	has_many   :invites,   :foreign_key => :attended_event_id
-	has_many   :attendees, through:        :invites
-	scope			 :upcoming, -> { where('date >= ?', DateTime.now).order('Date ASC') }
-	scope      :past,     -> { where('date < ?', DateTime.now).order('Date DESC') }
+	validates  :title,       presence: true
+	validates  :description, presence: true
+	validates  :date,        presence: true
+	belongs_to :creator,     :class_name  => "User"
+	has_many   :invites,     :foreign_key => :attended_event_id
+	has_many   :attendees,   through:        :invites
+	scope			 :upcoming,    -> { where('date >= ?', DateTime.now).order('Date ASC') }
+	scope      :past,        -> { where('date < ?', DateTime.now).order('Date DESC') }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	validates :name,  presence: true
+	validates :email, presence: true
 	has_many :created_events,  :foreign_key => "creator_id", :class_name => "Event"
 	has_many :invites,         :foreign_key => :attendee_id
 	has_many :attended_events, through:        :invites
