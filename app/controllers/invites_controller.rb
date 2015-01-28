337905f1012361062453b4153a52ff5f74c9b18b
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-28 14:56:53 -0800

    bootstrap styling

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,13 +3,13 @@ class InvitesController < ApplicationController
 	def create
 		@event = Event.find(params[:invite][:attended_event_id])
 		current_user.invites.create(attended_event_id: @event.id)
-		redirect_to @event
+		redirect_to current_user
 	end
 
 	def destroy
-		@event = Event.find(params[:invite][:attended_event_id])
-		current_user.invites.destroy(attended_event_id: @event.id)
-		redirect_to @event
+		@invite = Invite.find(params[:id])
+		current_user.invites.destroy(@invite)
+		redirect_to current_user
 	end
 
 end
